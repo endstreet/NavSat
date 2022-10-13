7bf2ddfd82adb8b1e10da73013f946009ea966c8
Johan Smit <endstreet@gmail.com>
2022-10-13 11:06:38 +0200

    Simplify Mapping Profile.

--- NavSat.Core/ApiClients/Profiles/GeoJsonProfile.cs
+++ NavSat.Core/ApiClients/Profiles/GeoJsonProfile.cs
@@ -9,17 +9,15 @@ using System.Xml.Serialization;
 
 namespace NavSat.Core.ApiClients.Profiles
 {
-    public class GeoJsonLineProfile : Profile
+    public class GeoJsonProfile : Profile
     {
 
-        public GeoJsonLineProfile()
+        public GeoJsonProfile()
         {
             CreateMap<SatellitePath, FeatureCollection>()
-            .ForMember(dest => dest.type, opt => opt.MapFrom(src => "FeatureCollection"))
             .ForMember(dest => dest.features, opt => opt.MapFrom(src => new List<Feature>()
             {
                 new Feature(){
-                    type = "Feature",
                     geometry = (src.Path.Count() == 1) ? new Geometry {type = "Point",coordinates = src.Path.Select(x =>  new List<double>{ x.Trace.Longitude, x.Trace.Latitude }).First() } : new Geometry { type = "LineString",coordinates = src.Path.Select(x =>  new List<double>{ x.Trace.Longitude, x.Trace.Latitude }).ToList() },
                     properties = new Properties
                     {
