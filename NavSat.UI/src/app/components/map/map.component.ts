d1504b594d46ce6465ab2958a98fd0de0f622e2c
Johan Smit <endstreet@gmail.com>
2022-10-16 21:53:57 +0200

    Map

--- NavSat.UI/src/app/components/map/map.component.ts
+++ NavSat.UI/src/app/components/map/map.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Loader } from '@googlemaps/js-api-loader';
+import { styles } from './mapstyles';
+
 
 @Component({
   selector: 'app-map',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+
+  title = 'google-maps';
+
+  private map!: google.maps.Map
+  private position!: GeolocationPosition
+
+  
 
   ngOnInit(): void {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((location) => {
+        this.position = location;
+      });
+    }
+    else {
+      //todo: default position
+    }
+    let loader = new Loader({
+    apiKey: 'AIzaSyDAmDOAJBNxAt_sfi_OOBE4c25AUio1GHQ',
+    });
+    loader.load().then(() => {
+        this.map = new google.maps.Map(document.getElementById("gmap")!, {
+          center: { lat: this.position.coords.latitude, lng: this.position.coords.longitude },
+          zoom: 6,
+          styles: styles
+        })
+        const marker = new google.maps.Marker({
+          position: { lat: this.position.coords.latitude, lng: this.position.coords.longitude },
+          map: this.map,
+        });
+      })
   }
-
 }
+
+
+
+
